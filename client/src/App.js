0906c3b4b4e5a17fbf4b433df46bbd6fdc924a4d
Sander Kaljula <sanderkaljula180@gmail.com>
2020-09-02 17:26:56 +0300

    dockerfile.dev

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Home from './Home';
+import logo from './logo.svg';
 import './App.css';
 
-const App = () => (
-  <Switch>
-    <Route exact path="/" component={Home} />
-  </Switch>
-);
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,16 +1,9 @@
-import App from './App';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import App from './App';
 
-describe('<App />', () => {
-  test('renders without exploding', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-      div
-    );
-  });
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
